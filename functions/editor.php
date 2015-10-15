e1466069322cbe974da047cc276e0e422d461220
Jaime Martinez <jmslbam@gmail.com>
2015-10-15 16:52:12 +0200

    Always remove the H1 from the WYSIWYG editor
    Because for website that we built, we control this ourselfs

---
+++ functions/editor.php
@@ -0,0 +1,42 @@
+<?php
+namespace Clarkson\WPADMIN\Editor;
+
+/***
+ *
+ * Configure the editor within wp-admin
+ *
+ **/
+
+class Editor
+{
+    protected $instance = null;
+
+    public static function get_instance()
+    {
+        static $instance = null;
+        if (null === $instance) {
+            $instance = new Editor();
+        }
+
+        return $instance;
+    }
+
+    protected function __construct(){
+
+        add_filter( 'tiny_mce_before_init', array($this, 'block_formats' ) );
+
+    }
+
+    /**
+     * Remove H1 from block drop-down
+     */
+    function block_formats( $init ){
+        $init['block_formats'] = "Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6;";
+        return $init;
+    }
+
+
+
+}
+
+Editor::get_instance();
