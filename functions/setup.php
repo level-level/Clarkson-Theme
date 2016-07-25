30a11ec52a06afa5101afbd0c7a34eec3fcbe62d
Jaime Martinez <jmslbam@gmail.com>
2016-07-25 17:57:02 +0200

    Add default theme-support code and primary menu

---
+++ functions/setup.php
@@ -0,0 +1,26 @@
+<?php
+namespace Clarkson\Setup;
+
+/**
+ * Theme setup
+ */
+function setup() {
+
+    // Enable plugins to manage the document title
+    // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
+    add_theme_support('title-tag');
+    // Register wp_nav_menu() menus
+    // http://codex.wordpress.org/Function_Reference/register_nav_menus
+    register_nav_menus([
+        'primary_navigation' => __('Primary Navigation', 'sage')
+    ]);
+    // Enable post thumbnails
+    // http://codex.wordpress.org/Post_Thumbnails
+    // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
+    // http://codex.wordpress.org/Function_Reference/add_image_size
+    add_theme_support('post-thumbnails');
+    // Enable HTML5 markup support
+    // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
+    add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+}
+add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
