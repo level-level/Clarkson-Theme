0512cf0c2fa03765466218a2003c5bbc314101b2
Jaime Martinez <jmslbam@gmail.com>
2016-07-25 17:56:16 +0200

    namespace the assets.php

--- functions/assets.php
+++ functions/assets.php
@@ -1,13 +1,13 @@
 <?php
+namespace Clarkson\Assets;
 
 function clarkson_add_editor_style(){
 	add_editor_style( get_template_directory_uri() . '/dist/styles/editor-style.css' );
 }
+add_filter( 'admin_init', __NAMESPACE__ . '\\clarkson_add_editor_style', 100 );
 
 function clarkson_add_assets(){
 	wp_enqueue_style('clarkson_main', get_template_directory_uri() . '/dist/styles/main.css', array(), '1.0.0', false );
 	wp_enqueue_script('clarkson_main', get_template_directory_uri() . '/dist/scripts/main.js', array(), '1.0.0', true);
 }
-
-add_action( 'admin_init', 		  'clarkson_add_editor_style', 100 );
-add_action( 'wp_enqueue_scripts', 'clarkson_add_assets'		 , 100 );
+add_filter( 'wp_enqueue_scripts', __NAMESPACE__ . '\\clarkson_add_assets', 100 );
