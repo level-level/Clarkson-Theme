4698ad9a9d2b3be76f0f35e0c4c0f50d66abf25a
Jaime Martinez <jmslbam@gmail.com>
2016-07-26 09:59:36 +0200

    Add Soil basics
    Yes yes, because you really really need to use this plugin

--- functions/setup.php
+++ functions/setup.php
@@ -6,6 +6,14 @@ namespace Clarkson\Setup;
  */
 function setup() {
 
+    // Enable features from Soil when plugin is activated.
+    // You should really use this plugin https://roots.io/plugins/soil/
+    add_theme_support('soil-clean-up');
+    add_theme_support('soil-disable-trackbacks');
+    add_theme_support('soil-js-to-footer');
+    add_theme_support('soil-nav-walker');
+    add_theme_support('soil-relative-urls');
+
     // Enable plugins to manage the document title
     // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
     add_theme_support('title-tag');
