85f6fbfc8c4976012507a92af3cbeda4d6265c66
Jaime Martinez <jmslbam@gmail.com>
2016-07-26 09:59:12 +0200

    Enclose JQuery in anonymous function

---
+++ source/scripts/main.js
@@ -0,0 +1,9 @@
+/* ========================================================================
+ * .noConflict()
+ * Enclosed everything within an anonymous function so that you can
+ * always reference jQuery with $, even when in .noConflict() mode.
+ * ======================================================================== */
+
+(function($) {
+
+})(jQuery); // Fully reference jQuery after this point.
