651c75f9c7f1a27c8a7390ed41c1b0e5c384f4ea
Jaime Martinez <jmslbam@gmail.com>
2016-10-18 14:08:52 +0200

    Add anchor links to headers

--- js/main.js
+++ js/main.js
@@ -129,5 +129,18 @@ function options () {
         });
     }
 
+    function init_anchored_header(){
+        return $("h2, h3, h4, h5, h6").each(function(i, el) {
+            var $el, icon, id;
+            $el = $(el);
+            id = $el.attr('id');
+            icon = '<i class="mini material-icons">toc</i>';
+            if (id) {
+                return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+            }
+        });
+    }
+
     init_toc();
+    init_anchored_header();
 })(jQuery);
