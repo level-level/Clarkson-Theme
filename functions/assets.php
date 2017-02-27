f8506a55ea5090979851057664d1f3250d33e336
Jaime Martinez <jmslbam@gmail.com>
2017-02-27 13:07:11 +0100

    depend main.js on jquery

--- functions/assets.php
+++ functions/assets.php
@@ -8,6 +8,6 @@ add_filter( 'admin_init', __NAMESPACE__ . '\\clarkson_add_editor_style', 100 );
 
 function clarkson_add_assets(){
 	wp_enqueue_style('clarkson_main', get_template_directory_uri() . '/dist/styles/main.css', array(), '1.0.0', false );
-	wp_enqueue_script('clarkson_main', get_template_directory_uri() . '/dist/scripts/main.js', array(), '1.0.0', true);
+	wp_enqueue_script('clarkson_main', get_template_directory_uri() . '/dist/scripts/main.js', array('jquery'), '1.0.0', true);
 }
 add_filter( 'wp_enqueue_scripts', __NAMESPACE__ . '\\clarkson_add_assets', 100 );
