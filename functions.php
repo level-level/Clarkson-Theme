79f4ea1d04c394f03e8cb4c901ea4fe900b84774
Jaime Martinez <jmslbam@gmail.com>
2017-05-30 12:02:52 +0200

    Trigger Error instead of Warning

--- functions.php
+++ functions.php
@@ -5,5 +5,5 @@ $autoloader_path = get_template_directory() . '/vendor/autoload.php';
 if( file_exists( $autoloader_path ) ){
     include_once( $autoloader_path );
 } else {
-    trigger_error(" Don't forget to run composer install.", E_USER_WARNING );
+    trigger_error(" Don't forget to run composer install.", E_USER_ERROR );
 }
