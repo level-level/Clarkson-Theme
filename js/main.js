ea4086b12d09ca854fbc1eb61bcdbc9b093eaec8
Jaime Martinez <jmslbam@gmail.com>
2018-07-12 15:44:41 +0200

    introduce new sub project navigation

--- js/main.js
+++ js/main.js
@@ -1,55 +1,68 @@
 (function($) {
     all();
     init_toc();
+    doc_toc();
 })(jQuery); // Fully reference jQuery after this point.
 function all () {
-//    $(".drag-target").remove();
-    var parts = window.location.pathname.split( '/' );
-    var type = parts[1];
-    if (type !== undefined && type != "") {
-        /*
-         Navigation bar highlighting
-         */
-        var currentPage = parts[2];
+    /**
+     * Navigation bar highlighting
+     * This doesn't take account for the third level in /core/docs/template.html
+     */
 
-        $(".j-nav li a").each(function() {
-            var $a = $(this).attr("href").split('/')[2];
-            if ($a == currentPage) {
-                $(this).parent().addClass("active");
+    $(".j-nav li a").each(function() {
+        var $href = $(this).attr("href");
+
+        //console.log(window.location.pathname, window.location.hostname, $href );
+
+        if ( window.location.pathname === $href )  {
+            $(this).parent().addClass("active");
+
+            // find the base of the current href to also select it's parent
+            $parts = $href.split("/");
+            if( $parts.length ){
+                $parent = '/' + $parts[1] + '/';
+                $( '.nav-main .j-nav li a[href="' + $parent + '"]').parent().addClass('active');
             }
-        });
-
-        /*
-         Documentation page highlighting
-         */
-        // First, check if we're on the right page
-        if ($("#docs").length) {
-            var currentDocPage = parts[3];
-            $(".docs li a").each(function(key) {
-                var $docPage = $(this).attr("href").split('/')[3];
-                if ($docPage == currentDocPage
-                    || currentDocPage == "" && key == 0
-                    || currentDocPage === undefined && key == 0) {
-                    $(this).addClass("active " + type +"-text");
-                }
-            });
         }
 
-        /*
-         Activate sideNav for mobile
-         */
-        $('.button-collapse').sideNav({
-            closeOnClick: true
-        });
-    }
+    });
+
+    /*
+        Activate sideNav for mobile
+        */
+    $('.button-collapse').sideNav({
+        closeOnClick: true
+    });
+
 }
 function init_toc(){
     $('.toc').toc({
         'selectors': 'h2', //elements to use as headings
-        'container': '.content-text', //element to find all selectors in
+        'container': '.main', //element to find all selectors in
         'smoothScrolling': true, //enable or disable smooth scrolling on click
         'itemClass': function(i, heading, $heading, prefix) { // custom function for item class
             return 'menu-item';
         }
     });
+}
+
+function doc_toc(){
+    $collection = $('ul.collection.docs');
+    $active = $collection.find('.active');
+    // if no active, then just fallback to first item
+    if ( $active && $active.length <= 0 ) {
+        $active = $collection.find('li:first-child');
+    }
+    li = '';
+    h2 = [];
+    $('.main h2').each( function(){
+        li = li + '<li><a class="collection-item" href="#' + this.id + '">' + $(this).html() + '</a></li>';
+        //h2[ this.id ] = $(this).text();
+    });
+
+    if( li ){
+        $active.after('<ul class="children">' + li + '</ul>');
+    }
+    console.log( $active, li );
+
 }
